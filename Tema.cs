6136c6289bd3520e108d05eecd121a576d08bde2
Ana Carolina Gonçalves <85302876+carolgbn@users.noreply.github.com>
2021-06-20 15:42:56 -0300

    Add files via upload

---
+++ Tema.cs
@@ -0,0 +1,17 @@
+namespace DIO.Series
+{
+	public enum Tema
+	{
+		Criação = 1,
+		Força = 2,
+		Gradidão = 3,
+		Fé = 4,
+		Cruz = 5,
+		Coragem = 6,
+		Oração = 7,
+		Obediência = 8,
+		Amor = 9,
+		Anizade = 10,
+		
+	}
+}
